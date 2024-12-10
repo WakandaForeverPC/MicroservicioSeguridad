ad6f7f0d86069dbb46acd64dc0729dd5c46795b8
Samuu10 <svallmun@myuax.com>
2024-12-10 18:27:15 +0100

    Initial commit

---
+++ src/test/java/com/proyecto/microservicioseguridad/MicroservicioSeguridadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.microservicioseguridad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioSeguridadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
